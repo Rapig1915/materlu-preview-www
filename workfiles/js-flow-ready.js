2f421851cacef9ab52ebb4ac31b84526dd395b8b
Rapig1915 <rapig1915@gmail.com>
2020-10-27 03:25:04 +0800

    change flipping library, responsive flipping done

--- workfiles/js-flow-ready.js
+++ workfiles/js-flow-ready.js
@@ -382,65 +382,20 @@ $("body").on("click",".step-character .edit-step", function() {
 	}
 });
 
-function initBooklet(loading, next, prev, book){
-    loading.hide();
-    next.show();
-    prev.show();
-    book.show().booklet({
-      name:               null,                            // name of the booklet to display in the document title bar
-      width:              1000,                             // container width
-      height:             500,                             // container height
-      speed:              600,                             // speed of the transition between pages
-      direction:          'LTR',                           // direction of the overall content organization, default LTR, left to right, can be RTL for languages which read right to left
-      startingPage:       0,                               // index of the first page to be displayed
-      easing:             'easeInOutQuad',                 // easing method for complete transition
-      easeIn:             'easeInQuad',                    // easing method for first half of transition
-      easeOut:            'easeOutQuad',                   // easing method for second half of transition
+var $appendToSample = $("#mybook_sample");
+var $appendToPreview = $("#mybook_preview");
+for(var i = 1; i < amount-2; i ++)
+{
+	$(`<div page-no="${i+1}"><img/></div>`).appendTo($appendToSample);
+	$(`<div page-no="${i+1}"><img/></div>`).appendTo($appendToPreview);
+}
 
-      closed:             true,                           // start with the book "closed", will add empty pages to beginning and end of book
-      closedFrontTitle:   null,                            // used with "closed", "menu" and "pageSelector", determines title of blank starting page
-      closedFrontChapter: null,                            // used with "closed", "menu" and "chapterSelector", determines chapter name of blank starting page
-      closedBackTitle:    null,                            // used with "closed", "menu" and "pageSelector", determines chapter name of blank ending page
-      closedBackChapter:  null,                            // used with "closed", "menu" and "chapterSelector", determines chapter name of blank ending page
-      covers:             false,                           // used with  "closed", makes first and last pages into covers, without page numbers (if enabled)
-
-      pagePadding:        0,                              // padding for each page wrapper
-      pageNumbers:        true,                            // display page numbers on each page
-
-      hovers:             false,                            // enables preview pageturn hover animation, shows a small preview of previous or next page on hover
-      overlays:           false,                            // enables navigation using a page sized overlay, when enabled links inside the content will not be clickable
-      tabs:               false,                           // adds tabs along the top of the pages
-      tabWidth:           60,                              // set the width of the tabs
-      tabHeight:          20,                              // set the height of the tabs
-      arrows:             false,                           // adds arrows overlayed over the book edges
-      cursor:             'pointer',                       // cursor css setting for side bar areas
-
-      hash:               false,                           // enables navigation using a hash string, ex: #/page/1 for page 1, will affect all booklets with 'hash' enabled
-      keyboard:           true,                            // enables navigation with arrow keys (left: previous, right: next)
-      next:               next,          			// selector for element to use as click trigger for next page
-      prev:               prev,          			// selector for element to use as click trigger for previous page
-
-      menu:               null,                            // selector for element to use as the menu area, required for 'pageSelector'
-      pageSelector:       false,                           // enables navigation with a dropdown menu of pages, requires 'menu'
-      chapterSelector:    false,                           // enables navigation with a dropdown menu of chapters, determined by the "rel" attribute, requires 'menu'
-
-      shadows:            true,                            // display shadows on page animations
-      shadowTopFwdWidth:  166,                             // shadow width for top forward anim
-      shadowTopBackWidth: 166,                             // shadow width for top back anim
-      shadowBtmWidth:     50,                              // shadow width for bottom shadow
-
-      before:             function(){},                    // callback invoked before each page turn animation
-      after:              function(){}                     // callback invoked after each page turn animation
-    });
-  }
-
-  var $appendToSample = $("#mybook_sample .b-load");
-  var $appendToPreview = $("#mybook_preview .b-load");
-  for(var i = 1; i < amount; i ++)
-  {
-    $(`<div page-no="${i+1}"><img/></div>`).appendTo($appendToSample);
-    $(`<div page-no="${i+1}"><img/></div>`).appendTo($appendToPreview);
-  }
-
-  initBooklet($('#loading_sample'), $('#next_page_button_sample'), $('#prev_page_button_sample'), $('#mybook_sample'));
-  initBooklet($('#loading_preview'), $('#next_page_button_preview'), $('#prev_page_button_preview'), $('#mybook_preview'));
+$(window).resize(function(){
+	const pageSize = (Math.min(1000, window.innerWidth-50)/2).toFixed(0);
+	$('#mybook_sample, #mybook_preview').booklet({ 
+		width: pageSize*2,
+		height: pageSize,
+		pageNumbers: false,
+		pagePadding: 0,
+	});
+}).trigger("resize")
